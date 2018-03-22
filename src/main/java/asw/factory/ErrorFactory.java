f2b360bc6752d339d26ef6cf841d5fb8e26d308e
Unknown <pelayo.garcia97@gmail.com>
2018-03-22 20:16:07 +0100

    ErrorFactory
    Case para Incorrect Login

--- src/main/java/asw/factory/ErrorFactory.java
+++ src/main/java/asw/factory/ErrorFactory.java
@@ -33,8 +33,8 @@ public class ErrorFactory {
 			return new UserNotFoundResponse();
 		case REQUIRED_LOGIN:
 			return new RequiredLoginErrorResponse();
-		
-
+		case INCORRECT_LOGIN:
+			return new UnknowErrorResponse();
 		default:// en caso de no conocer el error.
 			return new UnknownErrorResponse();
 		}
