9b5144bffb01c248062685be043b391f15b5deb0
Pablo Amorin Triana <pabloarrobaconb@gmail.com>
2018-05-05 18:59:16 +0200

    Update AgentsService.java

--- src/main/java/asw/services/AgentsService.java
+++ src/main/java/asw/services/AgentsService.java
@@ -12,7 +12,7 @@ import asw.dbManagement.entities.LoginPO;
 public class AgentsService {
 
 	public AgentPO checkUserAndPass(String user, String pass, String kind) {
-		final String uri = "http://ec2-54-245-139-175.us-west-2.compute.amazonaws.com:8081/user";
+		final String uri = "http://ec2-54-149-62-135.us-west-2.compute.amazonaws.com:8081";
 
 		RestTemplate restTemplate = new RestTemplate();
 
